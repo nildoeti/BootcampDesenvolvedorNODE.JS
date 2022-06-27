6a7d81b3feafc13525450afd88cf4c19e4377123
Nildo <nildo@webstack.com.br>
2022-06-27 11:41:48 -0300

    finalizado script do projeto app.js

--- grades-control-api/src/app.js
+++ grades-control-api/src/app.js
@@ -4,3 +4,4 @@ const port = 3000; // variável para a porta que será utilizada pela api
 
 app.get('/', (req, res) => res.send('Hello World!')); // rota da raiz
 app.listen(port, () => console.log(`App listening on port  ${port}!`)); // inicia a api
+
