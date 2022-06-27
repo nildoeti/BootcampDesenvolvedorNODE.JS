372989c4a0822b975456eb6de498e4feb27098ca
Nildo <nildo@webstack.com.br>
2022-06-27 07:42:55 -0300

    finalizado algoritimo para servidor http com node

---
+++ http/index.js
@@ -0,0 +1,11 @@
+// exemplo de servidor http com  node
+var http = require('http');
+http.createServer( function (req, res ) {
+	if ( (req.method === 'GET') && (req.url === '/test' )) {
+		res.write ('GET /test');
+	} else {
+		res.write ('Hello World');
+	}
+	res.statusCode = 200;
+	res.end();
+}).listen(8000);
